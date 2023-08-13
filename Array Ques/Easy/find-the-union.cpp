4882574d66c9822789d466415ab3f175d605396f
Siddhi Jain <76587795+silentspectator20@users.noreply.github.com>
2023-08-13 22:13:11 +0530

    Add files via upload

---
+++ Array Ques/Easy/find-the-union.cpp
@@ -0,0 +1,62 @@
+// Program to find the union of two sorted arrays of size n and m
+// Eg : Input:
+// n = 5, m = 5. arr1[] = {1, 2, 3, 4, 5} arr2[] = {2, 3, 4, 4, 5} 
+// Output:
+// { 1, 2, 3, 4, 5}
+// 
+// Brute Force: 
+//  Step 1: use a single map to store the frequency of elements in the array 1 and array 2 
+//  Step 2: Store the elements in the new array now
+
+// Optimal Approach: Two pointer approach
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> FindUnion(int arr1[], int arr2[], int n, int m)
+{
+    int i = 0, j = 0;  // pointers
+    vector<int> Union; // Uninon vector
+    while (i < n && j < m)
+    {
+        if (arr1[i] <= arr2[j]) // Case 1 and 2
+        {
+            if (Union.size() == 0 || Union.back() != arr1[i])
+                Union.push_back(arr1[i]);
+            i++;
+        }
+        else // case 3
+        {
+            if (Union.size() == 0 || Union.back() != arr2[j])
+                Union.push_back(arr2[j]);
+            j++;
+        }
+    }
+    while (i < n) // IF any element left in arr1
+    {
+        if (Union.back() != arr1[i])
+            Union.push_back(arr1[i]);
+        i++;
+    }
+    while (j < m) // If any elements left in arr2
+    {
+        if (Union.back() != arr2[j])
+            Union.push_back(arr2[j]);
+        j++;
+    }
+    return Union;
+}
+
+int main()
+
+{
+    int n = 10, m = 7;
+    int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+    vector<int> Union = FindUnion(arr1, arr2, n, m);
+    cout << "Union of arr1 and arr2 is  " << endl;
+    for (auto &val : Union)
+        cout << val << " ";
+    return 0;
+}
